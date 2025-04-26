f0926c5846b37ad6f6438917624747a54020c647
tpavay <pavayt@gmail.com>
2025-04-26 17:53:55 -0400

    FEAT: Create SwiftData model used to persist a ToDoItem

---
+++ ToDoApp/Models/ToDoItem.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoItem.swift
+//  ToDoApp
+//
+//  Created by Tyler Pavay on 4/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class ToDoItem {
+    var id = UUID().uuidString
+    var name: String
+    var desc: String
+    var dueDate: Date
+    var isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, name: String, desc: String, dueDate: Date, isCompleted: Bool) {
+        self.id = id
+        self.name = name
+        self.desc = desc
+        self.dueDate = dueDate
+        self.isCompleted = isCompleted
+    }
+}
